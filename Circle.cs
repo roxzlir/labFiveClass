4e4ab1d99e4cb76f031aeb68d6830f7e29a0a4ad
roxzlir <emil.ch.nordin@gmail.com>
2023-10-05 14:42:53 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labFiveClass
+{
+    internal class Circle
+    {
+        int _radius;
+        double _pi;
+        public Circle(int radius)
+        {
+            _radius = radius;
+            _pi = Math.PI;
+        }
+
+        public void GetArea()
+        {
+
+            Console.WriteLine(_radius * _radius * _pi);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace labFiveClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+            circle1.GetArea();
+            circle2.GetArea();
+        }
+    }
+}
