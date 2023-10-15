508cb32e68c598cfe00243a9793d4b164dbd79d5
roxzlir <emil.ch.nordin@gmail.com>
2023-10-15 12:21:25 +0200

    Added comments!

--- Program.cs
+++ Program.cs
@@ -5,16 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Vi ska räkna ut area på cirklar!");
-            
-            Circle circle1 = new Circle(GetInputNumber());
+            Console.WriteLine("Du får nu skriva in radien för en cirkel som du vill veta arean på.");
+            Circle circle1 = new Circle(GetInputNumber()); //Skapar ett objekt och kör metoden för inmatning för att få ett värde för int _radius
+            Console.WriteLine("Och nu en till!");
             Circle circle2 = new Circle(GetInputNumber());
             
-            
-            circle1.GetArea();
+
+            circle1.GetArea(); //Efter objekt är skapat kör vi klass metoden som kör matte formeln och skriver ut svaret
             circle2.GetArea();
         }
 
-        public static int GetInputNumber()
+        public static int GetInputNumber() //Skapar en metod för att felsökning av inmatning från användare så vi är säker på att vi får ett heltal.
         {
             Console.WriteLine("Vänligen skriv in vilken radie din cirkel har: ");
             int result;
